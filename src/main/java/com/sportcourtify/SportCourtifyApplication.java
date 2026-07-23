6a719fe5d0387e2f1becabe35b02e972e5fa44cf
Tuan Le <lctuan.0807@gmail.com>
2026-07-23 20:33:33 +0700

    SC-01 - project foundation setup

--- src/main/java/com/sportcourtify/SportCourtifyApplication.java
+++ src/main/java/com/sportcourtify/SportCourtifyApplication.java
@@ -1,13 +1,13 @@
-package com.sport_courtify.demo;
+package com.sportcourtify;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SportCourtifyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(SportCourtifyApplication.class, args);
 	}
 
 }
